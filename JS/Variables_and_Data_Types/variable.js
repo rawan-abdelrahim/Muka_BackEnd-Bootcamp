c10ff249503f76cd8dead29f3f312f81fab48f7d
rawan-abdelrahim <rawanabdo7575@gmail.com>
2024-10-26 22:19:28 +0300

    variables

---
+++ JS/Variables_and_Data_Types/variable.js
@@ -0,0 +1,15 @@
+// using let
+let age = 22;
+console.log("Age : ", age);
+age = 23;
+console.log("Age : ", age);
+
+// using cost
+const name = "RA_1";
+console.log("Name : ", name);
+
+//using var
+var city = "Cairo"
+console.log("City : ", city);
+city = "Port Said";
+console.log("City : ", city);
