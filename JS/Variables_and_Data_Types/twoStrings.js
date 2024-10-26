c0cd69d36b0ed7dccec37db87d6f1317b954c7ef
rawan-abdelrahim <rawanabdo7575@gmail.com>
2024-10-26 23:54:08 +0300

    compare Strings

---
+++ JS/Variables_and_Data_Types/twoStrings.js
@@ -0,0 +1,10 @@
+function compareStr(str1 , str2)
+{
+	if(str1 === str2)
+		console.log("The strings are equal.");
+	else
+		console.log("The strings are not equal.");
+}
+
+compareStr("Hello", "Hello");
+compareStrings("Hello", "World");
