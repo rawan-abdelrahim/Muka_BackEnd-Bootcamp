598d14675863428191cb7344ad10b922fd56b802
rawan-abdelrahim <rawanabdo7575@gmail.com>
2024-10-27 04:14:58 +0300

    get product

---
+++ JS/Functions/product.js
@@ -0,0 +1,10 @@
+function getproduct1(num1 , num2)
+{
+	return (num1 * num2);
+}
+
+const getproduct2 = (num1 , num2) => num1 * num2;
+
+
+console.log(getproduct1(5 , 7));
+console.log(getproduct2(5 , 7));
