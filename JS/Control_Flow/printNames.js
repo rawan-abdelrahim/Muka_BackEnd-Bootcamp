19c93da4ecab128293855354a7a98b6c7e8e11dd
rawan-abdelrahim <rawanabdo7575@gmail.com>
2024-10-27 14:26:59 +0300

    for loop

---
+++ JS/Control_Flow/printNames.js
@@ -0,0 +1,10 @@
+const printNames = (arr) => {
+	for(let i = 0; i < arr.length; i++)
+	{
+		console.log(arr[i]);
+	}
+
+};
+const names = ["Ichraf", "Youssef", "Ziad", "Ahmed", "Ibrahim"];
+
+printNames(names);
